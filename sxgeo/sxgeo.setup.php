597c878f1e772d53719b45bfa5ef110a70b3bed6
alexeevvlad <zzz-zzy@yadnex.ru>
2021-02-07 16:47:01 +0500

    Обновление базы данных для Sypex Geo от 2021.01.31

--- sxgeo/sxgeo.setup.php
+++ sxgeo/sxgeo.setup.php
@@ -6,10 +6,10 @@ Name=SxGeo IP base v2
 Category=mobile-geolocation
 Description=Integration of SxGeo IP base
 Version=1.0.1-2.2.4
-Date=2018.11.06 updated by alex.vlad
-Author=Andrey Matsovkin, sypexgeo.net/
-Copyright=Copyright (c) 2011-2013, Andrey Matsovkin, http://sypexgeo.net/
-Notes=Original GeoIP DB created by http://sypexgeo.net/ and distributed under BSD licence. <br />If your enjoy my plugin please consider donating to help support future developments. <b>Thanks!</b> <br /><a href="mailto:macik.spb@gmail.com">macik.spb@gmail.com</a>
+Date=2018.11.06
+Author=Andrey Matsovkin, sypexgeo.net, updated by alex.vlad
+Copyright=Copyright (c) 2011-2013, Andrey Matsovkin, http://sypexgeo.net
+Notes=Original GeoIP DB created by http://sypexgeo.net and distributed under BSD licence. <br />If your enjoy my plugin please consider donating to help support future developments. <b>Thanks!</b> <br /><a href="mailto:macik.spb@gmail.com">macik.spb@gmail.com</a>
 Auth_guests=R1
 Lock_guests=W2345A
 Auth_members=RW1
